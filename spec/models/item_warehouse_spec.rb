44f76a24f605d463e4e7e8a768b47cf84308dc91
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-14 20:28:19 -0600

    update .gitignore

--- spec/models/item_warehouse_spec.rb
+++ spec/models/item_warehouse_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe ItemWarehouse, type: :model do
   describe "relationships" do
     it { should belong_to(:item) }
-    it { should belong_to(:warehouse)) }
+    it { should belong_to(:warehouse) }
   end
 end
