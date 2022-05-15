d072abc4fe28ad52589a6cb1b7665a84a3961b1b
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-15 16:24:31 -0600

    Feat: Item edit page updates item info

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,23 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    @item.save
+
+    if @item.save
+      redirect_to "/items/#{@item.id}", alert: "#{@item.name} has been updated"
+    else
+      redirect_to "/items/#{@item.id}/edit"
+      flash[:alert] = "Error: #{error_message(@item.errors)}"
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price)
+  end
 end
