398e140b75cc893f75a228d01689497488847f1a
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-14 20:04:46 -0600

    Test: Item model relationships and validations

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Item, type: :model do
+  describe "relationships" do
+    it { should have_many(:item_warehouses) }
+    it { should have_many(:warehouses).through(:item_warehouses) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :unit_price }
+  end
+end
