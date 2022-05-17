c89676b811e2ce947864845fe900eb3005d724a1
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-17 16:47:34 -0600

    Feat: Warehouse index has button to create new warehouse

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -11,6 +11,20 @@ class WarehousesController < ApplicationController
     @warehouse = Warehouse.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+
+    if @warehouse.save
+      redirect_to "/warehouses/#{@warehouse.id}"
+    else
+      redirect_to "/warehouses/new"
+      flash[:alert] = "#{@warehouse.errors.full_messages.to_sentence}"
+    end
+  end
+
   private
 
   def warehouse_params
