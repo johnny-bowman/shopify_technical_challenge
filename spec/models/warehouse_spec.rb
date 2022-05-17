60fc6081e8131f7634ab0491f820f5d919af8e8a
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-17 15:43:33 -0600

    Test: Warehouse model finds warehouses by location

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe Warehouse, type: :model do
     it { should validate_presence_of :country }
     it { should validate_presence_of :postal_code }
   end
+
+  describe "class methods" do
+    before :each do
+      @wh_1 = Warehouse.create!(address: "123 Fake St", city: "Arlington", state: "VA", country: "USA", postal_code: "50890")
+      @wh_2 = Warehouse.create!(address: "987 Circles Ave", city: "Richmond", state: "VA", country: "USA", postal_code: "38475")
+    end
+
+    it "finds warehouses by location" do
+      expect(Warehouse.search("Arlington")).to eq([@wh_1])
+      expect(Warehouse.search("123 Fake St")).to eq([@wh_1])
+      expect(Warehouse.search("USA")).to eq([@wh_1, @wh_2])
+      expect(Warehouse.search("Richmond")).to eq([@wh_2])
+    end
+  end
 end
